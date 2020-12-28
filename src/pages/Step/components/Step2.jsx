560a4d8a0e166e16bd3ff584736c36db31a40980
chenhaoyun <chy405233403@outlook.com>
2020-12-28 15:49:43 +0800

    fix

--- src/pages/Step/components/Step2.jsx
+++ src/pages/Step/components/Step2.jsx
@@ -1,5 +1,5 @@
 import { PageContainer } from '@ant-design/pro-layout';
-import React, { useState, useEffect,useCallback,useMemo } from 'react';
+import React, { useState, useEffect,useCallback,useMemo,useRef } from 'react';
 import { Spin ,Button ,Select, message} from 'antd';
 import styles from './index.less';
 import {history} from 'umi';
@@ -32,9 +32,12 @@ export default (props) => {
     setNum2Val(num2Val)
     setNum3(num3)
     setNum3Val(num3Val);
+    localStorage.setItem('num1Val',num1Val);
+    localStorage.setItem('num2Val',num2Val);
+    localStorage.setItem('num3Val',num3Val);
     const step2=localStorage.getItem('step2');
     if(step2){
-      console.log(123,step2)
+      // console.log(123,step2)
       setallData(JSON.parse(step2))
     }
   }, []);
@@ -42,27 +45,46 @@ export default (props) => {
   const sum=()=>{
       // console.log(this.child.state)
       console.log(allData)
+      childRef.current.sumAll()
   } 
 
   const getData=(val)=>{
     console.log('子组件数据',val)
-    setallData(val)
+    var newArr=[];
+    val.map((item=>{
+      newArr.push({
+        ...item, 
+        name:1
+      })
+    }))
+    // setallData(newArr)
   }
 
   const edit=async()=>{
     // var quotations=[];
     var quotations=[];
-    allData.map(item=>{
+    var info=JSON.parse(localStorage.getItem('step2'))
+
+    info.map(item=>{
+      if(!item.has){
+        return false
+      }
       quotations.push({
-        bidder:item.name
+        // ...item,
+        bidder:item.name,
+        quotation:item.price,
+        manual_float_rate:parseFloat(item.float)+'',//手动下浮率%
+        other_score:item.other,
+        remark:item.remark
       })
     })
+    console.log('quotations',quotations)
     var parmas={
       quotations:JSON.stringify(quotations),
       tender_id:localStorage.getItem('tender_id'),
-      selected_adjustment_coefficient:num1,
-      selected_compound_coefficient:num2,
-      selected_float_coefficient:num3
+      selected_adjustment_coefficient:num1Val,
+      selected_compound_coefficient:num2Val,
+      selected_float_coefficient:num3Val
     }
     const result = await editTender_quotations(parmas);
     console.log('编辑结果',result);
@@ -75,14 +97,24 @@ export default (props) => {
   // const EditableTable=useMemo(()=>{
   //   <EditableTable   data={getData}/>
   // })
-  
+
+  const childRef = useRef();
+  // console.log(childRef.current.sumAll(99);)
+
+  const onRef=(ref)=>{
+    console.log(ref)
+    childRef.current=ref;
+  }
   return (
     <div className={styles.step2}>
           <div className={styles.top}>
             <span style={{fontSize:20,marginRight:20}}>预估抽球</span>
             <div>
               <span>调整系数</span>
-              <Select value={num1Val} style={{ width: '100px' }} onChange={(val)=>setNum1Val(val)} >
+              <Select value={num1Val} style={{ width: '100px' }} onChange={(val)=>{
+              setNum1Val(val)
+              localStorage.setItem('num1Val',val);
+              }} >
                 {num1.map(item=>
                   <Option key={item} value={item}>{item}</Option>
                 )}
@@ -90,7 +122,10 @@ export default (props) => {
             </div>
             <div>
               <span>复合系数</span>
-              <Select value={num2Val} style={{ width: '100px' }}   onChange={(val)=>setNum2Val(val)}>
+              <Select value={num2Val} style={{ width: '100px' }}   onChange={(val)=>{
+                setNum2Val(val)
+                localStorage.setItem('num2Val',val);
+              }}>
                 {num2.map(item=>
                   <Option key={item} value={item}>{item}</Option>
                 )}
@@ -98,21 +133,23 @@ export default (props) => {
             </div>
             <div>
               <span>下浮系数</span>
-              <Select value={num3Val} style={{ width: '100px' }}  onChange={(val)=>setNum3Val(val)}  >
+              <Select value={num3Val} style={{ width: '100px' }}  onChange={(val)=>{
+                setNum3Val(val)
+                localStorage.setItem('num3Val',val);
+              }}  >
                 {num3.map(item=>
                   <Option key={item} value={item}>{item}</Option>
                 )}
               </Select>
             </div>
           </div>
-          {allData.length>0&&  <EditableTable  msg={allData}  data={getData}/>}
-          {allData.length==0&&  <EditableTable  msg={allData}  data={getData}/>}
+          {/* {allData.length>0&&  <EditableTable  msg={allData}  data={getData}/>}
+          {allData.length==0&&  <EditableTable  msg={allData}  data={getData}/>} */}
+          {allData.length>0&&  <EditableTable  onRef={onRef}  data={getData}/>}
+          {allData.length==0&&  <EditableTable onRef={onRef}  data={getData}/>}
           <div style={{textAlign: 'right'}}>
             <Button  style={{ marginRight: 20 }} onClick={()=>{
               localStorage.setItem('step2',JSON.stringify(allData))
-              localStorage.setItem('num1Val',num1Val);
-              localStorage.setItem('num2Val',num2Val);
-              localStorage.setItem('num3Val',num3Val);
               history.go(-1)
             }}>上一步</Button>
             <Button  style={{ marginRight: 20 }} onClick={sum}>计算</Button>
--- src/pages/Step/components/Step2Table.js
+++ src/pages/Step/components/Step2Table.js
@@ -28,11 +28,14 @@ const EditableCell = ({
   ...restProps
 }) => {
   const [editing, setEditing] = useState(false);
+  const [oldVal, setOldVal] = useState();
   const inputRef = useRef();
   const form = useContext(EditableContext);
-  useEffect(() => {
+  useEffect(async() => {
     if (editing) {
       inputRef.current.focus();
+      const values = await form.validateFields();
+      setOldVal(values)
     }
   }, [editing]);
 
@@ -44,10 +47,15 @@ const EditableCell = ({
   };
 
   const save = async (e) => {
+    console.log(dataIndex)
     try {
+      
       const values = await form.validateFields();
+      console.log(2,oldVal,values,record);
+      var isEdit=(JSON.stringify(oldVal)!=JSON.stringify(values));
+      console.log(isEdit)
       toggleEdit();
-      handleSave({ ...record, ...values });
+      handleSave({ ...record, ...values ,dataIndex,isEdit});
     } catch (errInfo) {
       console.log('Save failed:', errInfo);
     }
@@ -103,7 +111,7 @@ export class EditableTable extends React.Component {
         title: '投标人',
         dataIndex: 'name',
         width: '5%',
-        editable: true,
+        // editable: true,
       },
       {
         title: '投标报价',
@@ -167,6 +175,8 @@ export class EditableTable extends React.Component {
         title: '总得分',
         dataIndex: 'num5',
         width: '4%',
+        render: (text, record) =>
+        <span>{record.num5&&parseFloat(record.num5).toFixed(2)}</span>
       },
       {
         title: '报价名次',
@@ -177,6 +187,8 @@ export class EditableTable extends React.Component {
         title: '报价分',
         dataIndex: 'num7',
         width: '4%',
+        render: (text, record) =>
+        <span>{record.num7&&parseFloat(record.num7).toFixed(2)}</span>
       },
       {
         title: '偏差值',
@@ -230,12 +242,66 @@ export class EditableTable extends React.Component {
     });
   };
   handleSave = (row) => {
+    console.log(row,!row.isEdit)
+    if(!row.isEdit){
+      console.log('不用编辑')
+      return false
+    }
     const newData = [...this.state.dataSource];
     const index = newData.findIndex((item) => row.key === item.key);
     const item = newData[index];
     newData.splice(index, 1, { ...item, ...row });
+
+    // if(!Number.prototype._toFixed) {    Number.prototype._toFixed = Number.prototype.toFixed;}Number.prototype.toFixed =function(n) {return(this + 1e-14)._toFixed(n);};
+
+    //投标预算价
+    var budgetPrice=parseFloat(JSON.parse(localStorage.getItem('step1')).price)
+    var newArr=[];
+    if(row.dataIndex=='price'){
+      newData.map(item=>{
+        newArr.push({
+          ...item, 
+          num1:null,
+          num2:null,
+          num3:null,
+          num4:null,
+          num5:null,
+          num6:null,
+          num7:null,
+          num8:null,
+          float:item.price?((budgetPrice-item.price)/budgetPrice*100).toFixed(3)+'%':null
+        })
+      })
+    }
+
+    if(row.dataIndex=='other'){
+      newData.map(item=>{
+        newArr.push({
+          ...item, 
+          num1:null,
+          num2:null,
+          num3:null,
+          num4:null,
+          num5:null,
+          num6:null,
+          num7:null,
+          num8:null,
+        })
+      })
+    }
+
+
+    if(row.dataIndex=='remark'){
+      newData.map(item=>{
+        newArr.push({
+          ...item, 
+        })
+      })
+    }
+ 
+    console.log('编辑后',row,newData,budgetPrice)
     this.setState({
-      dataSource: newData,
+      dataSource: newArr,
     });
   };
 
@@ -244,15 +310,202 @@ export class EditableTable extends React.Component {
   //   return true;
   // }
   componentDidMount=()=>{
-    console.log(1129,this.props.msg)
+    // console.log(1129,this.props.msg)
+    // this.setState({
+    //   dataSource:this.props.msg
+    // })
+    var step2=localStorage.getItem('step2')?JSON.parse(localStorage.getItem('step2')):[];
     this.setState({
-      dataSource:this.props.msg
+      dataSource:step2
     })
+
+    this.props.onRef(this)
   }
   componentDidUpdate=()=>{ 
-    console.log(233333,this.state.dataSource)
+    console.log('表格数据',this.state.dataSource)
+
+    // var newArr=[];
+    // this.state.dataSource.map(item=>{
+ 
+    //   newArr.push({
+    //     ...item, 
+    //     float:item.price?(budgetPrice-item.price)/budgetPrice/100+'%':null
+    //   })
+    // })
     // console.log(1130,this.props.msg)
-    this.props.data(this.state.dataSource);
+    // this.props.data(this.state.dataSource);
+  }
+  //计算
+  sumAll=()=>{
+    console.log('sumAll',this.state.dataSource)
+
+    //A=控制价=投标预算价*调整系数（调整系数由开标的时候随机抽取）
+    const A=parseFloat(JSON.parse(localStorage.getItem('step1')).price)*localStorage.getItem('num1Val');
+    //K=复合系数=0.3/0.35/0.4(这个由开标的时候随机获取）
+    const K=localStorage.getItem('num2Val');
+    //i=下浮系数=1,2,3（这个由开标的时候随机获取）
+    const i=localStorage.getItem('num3Val');
+    console.log('A',A,'k',K,'i',i)
+    //B=平均值
+    let B;
+    var arr=[...this.state.dataSource];
+    var numArr=[];
+    var num=0;
+    var price=0;
+     //十个区间
+    var intervalArr=[];
+    var intervalNumArr=[[],[],[],[],[],[],[],[],[],[]];
+    arr.map(item=>{
+      num++;
+      price+=parseFloat(item.price);
+      numArr.push(parseFloat(item.price))
+    })
+    if(num<=5){
+      B=price/num
+    }else if(num<=12){
+      console.log(numArr);
+      numArr.sort((a,b)=>
+        a-b
+      )
+      //去掉最小值
+      numArr.shift()
+      //去掉最大值
+      numArr.pop();
+      var price2=0;
+      numArr.map(item=>{
+        price2+=parseFloat(item);
+      })
+      B=price2/(num-2)
+
+    }else if(num>12){
+      numArr.sort((a,b)=>
+        a-b
+      )
+      console.log('原始值',numArr)
+      //去掉最小值
+      var minNum=numArr.shift()
+      //去掉最大值
+      var maxNum=numArr.pop();
+      var difference = (numArr[numArr.length-1]-numArr[0])/10;
+      console.log('区间值',difference)
+     
+      for (let j = 0; j < 10; j++) {
+        intervalArr.push([numArr[0]+j*difference,numArr[0]+(j+1)*difference])
+      }
+      
+      numArr.map((item,itemIndex)=>{
+        intervalArr.map((atem,index)=>{
+          if(item>=atem[0]&&item<atem[1]){
+            intervalNumArr[index].push(item)
+          }
+        })
+        if(itemIndex==numArr.length-1){
+          intervalNumArr[9].push(item)
+        }
+      })
+
+      var interIndex=0;
+      var allInterPrice=0;
+      intervalNumArr.map(item=>{
+        var interPirce=0;
+        if(item.length>0){
+          console.log(233,item)
+          interIndex++;
+          var allPrice=0;
+          item.map(atem=>{
+            allPrice+=atem
+          })
+          interPirce=allPrice/item.length;
+          allInterPrice+=interPirce
+        }
+      })
+      console.log(allInterPrice,interIndex)
+      B=allInterPrice/interIndex;
+      console.log('十个区间',intervalArr,intervalNumArr)
+
+    }
+    console.log('平均值',num,B);
+    const C=A*K+B*(1-K)*(100-i)/100;
+    console.log('基准价',C);
+
+    var newTableArr=[];
+    //总名次数组
+    var num4Arr=[];
+    //报价分名次数组
+    var num6Arr=[];
+    //偏差值
+    arr.map(item=>{
+
+      //去掉最小值和最大值的计算
+      if(parseFloat(item.price)==minNum||parseFloat(item.price)==maxNum){
+        newTableArr.push({
+          ...item
+        })
+        return false
+      }
+  
+      //区间位置
+      var num3=1;
+      if(intervalArr.length==10){
+        intervalArr.map((atem,index)=>{
+          if(parseFloat(item.price)>=atem[0]&&parseFloat(item.price)<atem[1]){
+            num3=index+1
+          }
+          if(parseFloat(item.price)==intervalArr[9][1]){
+            num3=10
+          }
+        })
+      }
+      //偏差值
+      var num8=(item.price-C)/C*100;
+      //报价分
+      var num7=item.price>C?(98-num8*1.2):(98+num8*1.0);
+      //总得分
+      var num5=num7+(item.other?parseFloat(item.other):0)
+      num4Arr.push(num5);
+      num6Arr.push(num7)
+  
+      newTableArr.push({
+        ...item,
+        //偏差值
+        num8:num8.toFixed(2)+'%',
+        //报价分
+        num7:num7,
+        //总得分
+        num5:num5,
+        //区间位置
+        num3:num3
+      })
+    })
+    var newTableArr2=[];
+    num4Arr.sort((a,b)=>b-a);
+    num6Arr.sort((a,b)=>b-a)
+    console.log('num4Arr',num4Arr,num6Arr)
+    newTableArr.map(item=>{
+      var num4,num6;
+      num4Arr.map((atem,index)=>{
+        if(atem==parseFloat(item.num5)){
+          num4=index+1
+        }
+      })
+      num6Arr.map((atem,index)=>{
+        if(atem==parseFloat(item.num7)){
+          num6=index+1
+        }
+      })
+      newTableArr2.push({
+        ...item,
+        num4:num4,
+        num6:num6,
+      })
+    })
+    console.log('newTableArr2',newTableArr2)
+    this.setState({
+      dataSource:newTableArr2
+    })
+    
+    
+    localStorage.setItem('step2',JSON.stringify(this.state.dataSource))
   }
   render() {
     const { dataSource } = this.state;
@@ -287,11 +540,13 @@ export class EditableTable extends React.Component {
           bordered
           dataSource={dataSource}
           columns={columns}
+          pagination={false}
         />
         <Button
           onClick={this.handleAdd}
           type="primary"
           style={{
+            marginTop: 16,
             marginBottom: 16,
           }}
         >
